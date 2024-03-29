fdbbd9c9eb345cfc3cf1b8b6a827685dc63332d5
Yuvadi29 <adityatrivedi612@gmail.com>
2024-03-29 17:38:31 +0530

    Functionality: Validation of Existing User Added ✅

--- server/Controller/user.controller.js
+++ server/Controller/user.controller.js
@@ -2,20 +2,27 @@ const User = require("../Model/User");
 
 const signup = async (req, res) => {
     try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email: email });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "User Already Exists" });
+        }
+
         const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
+            name,
+            email,
+            password,
         });
 
         await user.save();
-        res.status(200).json({ message: "Data Saved", data: user });
+        res.sendStatus(200).json({ message: "Data Saved", data: user });
     } catch (error) {
         console.log("Error saving user to database: ", error);
-        res.status(401).json({ message: error });
     }
 
-    return res.status(201).json({ message: User });
+    return res.sendStatus(201).json({ message: User });
 };
 
 
